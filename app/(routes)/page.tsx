4c69a79f91721b30365ffe4224c9fa4794ad417d
theanh1508 <85997901+0862551396@users.noreply.github.com>
2024-03-14 22:32:57 +0700

    build: setup

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,19 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("59af47d9-3d59-4363-bb2d-73885b4a77b2");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ components/ui/button.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from "react";
+import { cn } from "@/libs/utils";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          `w-auto rounded-full bg-black border-transparent px-5 py-3  text-white font-semibold transition`,
+          "disabled:cursor-not-allowed disabled:opacity-50 hover:opacity-75",
+          className
+        )}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
